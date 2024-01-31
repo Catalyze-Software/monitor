66c1cae3a9cf8aa46d721e99dca8b2f08afdbcbd
Web3NL <samerweb3@gmail.com>
2024-01-31 15:20:49 +0100

    init gix and chartist graph

---
+++ src/dashboard/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Content, Layout, MenuItem } from "@dfinity/gix-components"
+</script>
+
+<Layout>
+  <svelte:fragment slot="menu-logo"><span></span></svelte:fragment>
+  <svelte:fragment slot="menu-oneliner"><span></span></svelte:fragment>
+  <svelte:fragment slot="menu-items">
+    <MenuItem href="/" on:click>Overview</MenuItem>
+    <MenuItem href="/canisters" on:click>Canisters</MenuItem>
+    <MenuItem href="/logs" on:click>Logs</MenuItem>
+  </svelte:fragment>
+  <Content>
+    <h1 slot="title">Catalyze Dashboard</h1>
+    <main>
+      <slot />
+    </main>
+  </Content>
+</Layout>
+
+<style lang="scss" global>
+  @import "../../node_modules/@dfinity/gix-components/dist/styles/global.scss";
+</style>
