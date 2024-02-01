243a7ad79cfaa5fa97a4bfec1db0f1b1d5382d5c
Web3NL <samerweb3@gmail.com>
2024-02-01 20:20:40 +0100

    auth

--- src/monitor/src/utils/auth.rs
+++ src/monitor/src/utils/auth.rs
@@ -1,5 +1,5 @@
 // Allowed principals
-const ALLOWED_PRINCIPALS: [&str; 3] = [
+const ALLOWED_PRINCIPALS: [&str; 4] = [
     // Catalyze development principal
     "syzio-xu6ca-burmx-4afo2-ojpcw-e75j3-m67o5-s5bes-5vvsv-du3t4-wae",
     
@@ -7,7 +7,10 @@ const ALLOWED_PRINCIPALS: [&str; 3] = [
     "nvifv-62idm-izjcy-rvy63-7tqjz-mg2d7-jiw6m-soqvp-hdayh-mnqf5-yqe",
 
     // Samer II at Dashboard (https://ca77u-aiaaa-aaaap-abxiq-cai.icp0.io)
-    "bqhkz-ly6mk-7zcms-udvhw-hd7xy-mrlgx-gztdb-i55tr-wpsdz-7g6qc-wae"
+    "bqhkz-ly6mk-7zcms-udvhw-hd7xy-mrlgx-gztdb-i55tr-wpsdz-7g6qc-wae",
+
+    // Remco II at Dashboard (https://ca77u-aiaaa-aaaap-abxiq-cai.icp0.io)
+    "q3m5g-guuii-gy5u3-qekcy-rwqxq-ym3ff-zqjue-mjxue-2icoi-qcyk3-pqe"
 ];
 
 pub fn is_authenticated() -> Result<(), String> {
